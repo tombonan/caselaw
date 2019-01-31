77e4bbe19d3f4f94fabf2096ccc3e6a84cfc023c
tombonan <tombonan018@gmail.com>
2019-01-30 22:30:58 -0700

    Add webmock for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require "bundler/setup"
+require 'webmock'
+require 'webmock/rspec'
 require "caselaw"
 
 RSpec.configure do |config|
@@ -12,4 +14,24 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+def stub(path, fixture_name)
+  root = "https://api.case.law/v1/"
+  path = root + path
+  stub_request(:get, path).
+         with(
+           headers: {
+       	  'Authorization'=>'Token SECRET_KEY'
+           })
+         .to_return(status: 200, body: fixture(fixture_name), headers: {"Content-Type"=> "application/json"})
+end
+
+def fixture_path(file = nil)
+  path = File.expand_path("../fixtures", __FILE__)
+  file.nil? ? path : File.join(path, file)
+end
+
+def fixture(file)
+  file = File.read(fixture_path(file))
 end
